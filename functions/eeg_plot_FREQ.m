03a7d986291dd5c41f66266320e673f5fdd8fedb
brg015 <br.geib33@gmail.com>
2017-05-02 11:39:14 -0400

    Add files via upload

--- functions/eeg_plot_FREQ.m
+++ functions/eeg_plot_FREQ.m
@@ -55,26 +55,48 @@ load(RUN.template.plt3);
 % cfg_freq.pad        = 'maxperlen';
 % suffix='_ENS_DPSS_v2';
 
+% cfg_freq            = [];
+% cfg_freq.keeptrials = 'yes';
+% cfg_freq.output     = 'pow';
+% cfg_freq.method     = 'mtmconvol';
+% cfg_freq.taper = 'dpss';
+% cfg_freq.foi =  logspace(log10(4),log10(30),34);
+% cfg_freq.toi        = -1:0.05:1.7;
+% cfg_freq.tapsmofrq  = 5*log10(cfg_freq.foi);
+% cfg_freq.t_ftimwin =   [3./cfg_freq.foi(cfg_freq.foi<=3) ...
+%     4./cfg_freq.foi(cfg_freq.foi> 3 & cfg_freq.foi<=7) ...
+%     5./cfg_freq.foi(cfg_freq.foi> 7 & cfg_freq.foi<=14) ...
+%     7./cfg_freq.foi(cfg_freq.foi> 14 &  cfg_freq.foi<=20) ...
+%     10./cfg_freq.foi(cfg_freq.foi> 20 &  cfg_freq.foi<=50)];
+% cfg_freq.keeptapers = 'no';
+% cfg_freq.pad        = 'maxperlen';
+% suffix='_ENS_DPSS_v3';
+
 cfg_freq            = [];
 cfg_freq.keeptrials = 'yes';
-cfg_freq.output     = 'pow';
+cfg_freq.output     = 'fourier';
 cfg_freq.method     = 'mtmconvol';
-cfg_freq.taper = 'dpss';
-cfg_freq.foi =  logspace(log10(4),log10(30),34);
+cfg_freq.taper = 'hanning';
+cfg_freq.foi =  logspace(log10(4),log10(16.2911),24);
 cfg_freq.toi        = -1:0.05:1.7;
-cfg_freq.tapsmofrq  = 5*log10(cfg_freq.foi);
-cfg_freq.t_ftimwin =   [3./cfg_freq.foi(cfg_freq.foi<=3) ...
+cfg_freq.t_ftimwin = [3./cfg_freq.foi(cfg_freq.foi<=3) ...
     4./cfg_freq.foi(cfg_freq.foi> 3 & cfg_freq.foi<=7) ...
     5./cfg_freq.foi(cfg_freq.foi> 7 & cfg_freq.foi<=14) ...
-    7./cfg_freq.foi(cfg_freq.foi> 14 &  cfg_freq.foi<=20) ...
-    10./cfg_freq.foi(cfg_freq.foi> 20 &  cfg_freq.foi<=50)];
-cfg_freq.keeptapers = 'no';
+    7./cfg_freq.foi(cfg_freq.foi> 14 &  cfg_freq.foi<=20)];
+cfg_freq.keeptapers = 'yes';
 cfg_freq.pad        = 'maxperlen';
-suffix='_ENS_DPSS_v3';
+cfg_freq.type       = 'coherence';
+suffix='_ENS_COH_v4';
 
-for iSubj=2:length(RUN.dir.subjects)
+for iSubj=1:length(RUN.dir.subjects)
     if RUN.dir.plot(iSubj)==0, continue; end
-    if ~exist(fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB' suffix '.mat']),'file')
+    if strcmp(cfg_freq.output,'fourier')
+        save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_ITC' suffix '.mat']);
+    else
+        save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB' suffix '.mat']);
+    end
+    if ~exist(save_file,'file')
+        clear save_file; % It's reset later
         % Load in data
         load(fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '.mat']));
         load(fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_booleans.mat']));
@@ -84,44 +106,58 @@ for iSubj=2:length(RUN.dir.subjects)
 
         data=ft_remove_ems(data,(data.reject | data.lat_rej30));
 %         csd_data=ft_scalpcurrentdensity([],data);
-        freqlock=ft_freqanalysis(cfg_freq, data);
+
+        freqlock=ft_freqanalysis(cfg_freq, data); clear data;
         
-        cfg_f.baselinetype='ztransformSession';
-        cfg_f.inc_trials=~logical(data.reject | data.lat_rej30);
-        cfg_f.baseline=[-.5 .2]; % Not used to z-transform
-        cfg_f.ztrials=(booleans.Rem | booleans.For);
-        Fz=ft_freqbaseline(cfg_f,freqlock);
-        
-        cfg_f.baselinetype='logpower';
-        cfg_f.inc_trials=~logical(data.reject | data.lat_rej30);
-        cfg_f.baseline=[-.5 .2]; % Not used to z-transform
-        dB=ft_freqbaseline(cfg_f,freqlock);
-        frej=logical(data.reject | data.lat_rej30);
-        for ii=1:2
-            switch ii
-                case 1, fdata=Fz; save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_Fz' suffix '.mat']);
-                        save_file2=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_Fz_all' suffix '.mat']);
-                case 2, fdata=dB; save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB' suffix '.mat']);
-                        save_file2=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB_all' suffix '.mat']);
-            end
-            save(save_file2,'fdata','frej');
-            
-            for jj=1:length(fn)
-                sfdata.(fn{jj}){1}=fdata;
-                if sum(~booleans.(fn{jj}))~=length(booleans.(fn{jj}))
-                    sfdata.(fn{jj}){1}.powspctrm(~booleans.(fn{jj}),:,:,:)=[];
-                    % Can't save trials here, so simplify
-                    sfdata.(fn{jj}){1}=ft_freqdescriptives([],sfdata.(fn{jj}){1});
-                else
-                    sfdata.(fn{jj}){1}=[]; 
-                end
-            end
-            save(save_file,'sfdata'); clear sfdata fdata;
+        if strcmp(cfg_freq.output,'fourier')
+            ITC = ft_ITC(booleans,freqlock);
+            save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_ITC' suffix '.mat']);
+            save(save_file,'ITC'); clear ITC freqlock;
         end
+        
+        if strcmp(cfg_freq.output,'pow')
+            cfg_f.baselinetype='ztransformSession';
+            cfg_f.inc_trials=~logical(data.reject | data.lat_rej30);
+            cfg_f.baseline=[-.5 .2]; % Not used to z-transform
+            cfg_f.ztrials=(booleans.Rem | booleans.For);
+            Fz=ft_freqbaseline_custom(cfg_f,freqlock);
+
+            cfg_f.baselinetype='logpower';
+            cfg_f.inc_trials=~logical(data.reject | data.lat_rej30);
+            cfg_f.baseline=[-.5 .2]; % Not used to z-transform
+            dB=ft_freqbaseline_custom(cfg_f,freqlock);
+            frej=logical(data.reject | data.lat_rej30);
+            for ii=1:2
+                switch ii
+                    case 1, fdata=Fz; save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_Fz' suffix '.mat']);
+                            save_file2=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_Fz_all' suffix '.mat']);
+                    case 2, fdata=dB; save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB' suffix '.mat']);
+                            save_file2=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_dB_all' suffix '.mat']);
+                end
+                save(save_file2,'fdata','frej');
+
+                for jj=1:length(fn)
+                    sfdata.(fn{jj}){1}=fdata;
+                    if sum(~booleans.(fn{jj}))~=length(booleans.(fn{jj}))
+                        sfdata.(fn{jj}){1}.powspctrm(~booleans.(fn{jj}),:,:,:)=[];
+                        % Can't save trials here, so simplify
+                        sfdata.(fn{jj}){1}=ft_freqdescriptives([],sfdata.(fn{jj}){1});
+                    else
+                        sfdata.(fn{jj}){1}=[]; 
+                    end
+                end
+                save(save_file,'sfdata'); clear sfdata fdata;
+            end
+        end % Power
     end
 end
 
 N=sum(RUN.dir.plot);
+%=========================================================================%
+%% Freq GA
+%=========================================================================%
+if strcmp(cfg_freq.output,'pow')
+    
 if ~exist(fullfile(RUN.dir.sav,['GA_N' num2str(N) '_freq_ENS.mat']),'file')
     [ga_freq_dB,ga_freq_Fz]=eeg_ga_freq(suffix);
     fn=fieldnames(ga_freq_dB);
@@ -137,3 +173,23 @@ if ~exist(fullfile(RUN.dir.sav,['GA_N' num2str(N) '_freq_ENS.mat']),'file')
     save(fullfile(RUN.dir.sav,['GA_N' num2str(N) '_freq' suffix '.mat']),'ga_freq_dB','ga_freq_Fz');
 end
 
+end
+%=========================================================================%
+%% ITC GA
+%=========================================================================%
+if strcmp(cfg_freq.output,'fourier')
+    if ~exist(fullfile(RUN.dir.sav,['GA_N' num2str(N) '_freq_ENS_ITC.mat']),'file')
+        c=1;
+        for iSubj=1:length(RUN.dir.subjects)
+            if RUN.dir.plot(iSubj)==0, continue; end
+            save_file=fullfile(RUN.dir.pro,[RUN.dir.subjects{iSubj} '_ITC' suffix '.mat']);
+            load(save_file);
+            fn=fieldnames(ITC.itpc);
+            for jj=1:length(fn)
+                GA.(fn{jj})(c,:,:,:)=ITC.itpc.(fn{jj});
+            end
+            c=c+1;
+        end
+    end
+    save(fullfile(RUN.dir.sav,['GA_N' num2str(N) '_ITC' suffix '.mat']),'GA');
+end
