7df7ccb82fb20c1ce0021fb0b300fd64e757fd55
Benjamin Geib <br.geib33@gmail.com>
2020-05-11 13:25:11 -0400

    Add files via upload

--- ft_functions/erp_ss_plot_v3.m
+++ ft_functions/erp_ss_plot_v3.m
@@ -21,9 +21,18 @@ if ~isfield(tstruct,'filter'), tstruct.filter.on=0; end
 if ~isfield(tstruct,'delta'), tstruct.delta=0; end
 if ~isfield(tstruct,'legend'), tstruct.legend=f; end
 if ~isfield(tstruct,'fig'), tstruct.fig=true(1,5); end
+if ~isfield(tstruct,'SE'), tstruct.SE.on=0; end
 if length(tstruct.fig)<5, tstruct(5)=0; end
 if tstruct.filter.on==1, cfg=tstruct.filter; end
 
+lim=0.05; seq=3;
+tstruct.fig=[1 1 1 1 1]; 
+
+if tstruct.SE.on==1
+    if tstruct.SE.diff==0, SErange=1:(length(f)+1); end
+    if tstruct.SE.diff==1, SErange=3; end
+end
+        
 tstruct.subj=logical(tstruct.subj);
 time=tstruct.time;
 %-------------------------------------------------------------------------%
@@ -90,19 +99,19 @@ for ii=1:length(dat)
 end
 if tstruct.delta==1, b{3}=b{1}-b{2}; end
 t=dat{1}.time(i1);
+
 % Save the key outputs from this
 DATA=b; TIME=t;
 %=========================================================================%
 % Paper Figures...
 %=========================================================================%
 if tstruct.fig(2)==1
-
     figure(2);
 if length(f)==2
     % subplot(2,2,2);
-    cset={'k','k',[0 0 0]};
-    wset=[5 5 1];
-    lset={'-',':','-.'};
+    cset={'b','r',[0 0 0]};
+    wset=[2 2 5];
+    lset={'-','-',':'};
 else
     cset={'b' 'r' 'g' 'k' 'c' 'm'};
     wset=[5 5 5 5 5 5];
@@ -114,7 +123,7 @@ set(gcf,'color','w');
 lsize=14;
 hwidth=3;
 
-xl='uV/cm^2';
+xl='uV';
 for ii=1:length(b)
     if sum(tstruct.subj)>1
         plot(t.*1000,mean(b{ii}),'color',cset{ii},'linewidth',wset(ii),'linestyle',lset{ii}); hold on;
@@ -123,18 +132,105 @@ for ii=1:length(b)
     end
 end
 
+% Add SE confidence on
+if tstruct.SE.on==1
+    for ii=SErange
+        xconf=[t.*1000 t(end:-1:1)*1000];
+        Y=mean(b{ii}); Yerr=std(b{ii})./sqrt(size(b{ii},1));
+        yconf=[Y+Yerr Y(end:-1:1)-Yerr(end:-1:1)];
+        p=fill(xconf,yconf,cset{ii});
+        p.FaceAlpha=0.2;
+        p.EdgeColor='none';
+        clear xconf Y yconf p;
+    end
+end
+
+SSData.b=b;
+SSData.time=t;
+
 xlabel('Time (ms)','FontSize',lsize); ylabel(xl,'FontSize',lsize);
 legend(tstruct.legend,'location','SouthEast'); 
 % set(gca,'Ylim',[-2.5 .5]);
 set(gca,'FontSize',lsize);
 set(gca,'FontWeight','bold');
+
+%---------------------------------------------------------------------%
+SEQ =[];
+% * for ERP data also wanna subsample
+% b{3} is our difference wave to subsample on
+fs=0.05; c=1; % sample every 50ms
+for ii=TIME(1):fs:TIME(end)
+   A(:,c)=mean(b{3}(:,(TIME>=ii & TIME<ii+fs)),2);
+   T(c)=ii;
+   c=c+1;
+end
+[~,p,~,~]=ttest(A);
+
+AB=p; 
+AB=AB<lim; 
+c=0; found=0;
+for ii=1:length(AB)
+    if (AB(ii)==0 && found==0), continue;
+    elseif (AB(ii)==0 && found==1), found=0;
+    elseif (AB(ii)==1 && found==0)
+        found=1; c=c+1;
+        SEQ(c).L=1;
+        SEQ(c).I=ii;
+    elseif (AB(ii)==1 && found==1)
+        SEQ(c).L=SEQ(c).L+1;
+        SEQ(c).I=[SEQ(c).I ii];
+    end
+end
+% SEQ(1).I(end)=[];
+
+IND=false(size(AB));
+for ii=1:length(SEQ)
+    if SEQ(ii).L>=seq, IND(SEQ(ii).I)=1; end
+end
+% plot(AB,'b'); hold on; plot(IND,'r'); hold on; plot(K,'k');
+for ii=1:length(SEQ)
+    if SEQ(ii).L>=seq
+        a=get(gca,'Ylim'); alow=T(SEQ(ii).I(1))*1000; ahgh=T(SEQ(ii).I(end))*1000; acol=[.9 .9 .9]; 
+        hline=patch([alow ahgh ahgh alow alow],[a(1) a(1) a(2) a(2) a(1)],acol,'EdgeColor','none','FaceAlpha',1);
+        % hline=line([0 0],[get(gca,'Ylim')]); set(hline,'color',[.7 .7 .7]); set(hline,'linewidth',3);
+    end
+    SEQ(ii).T=[T(SEQ(ii).I(1))*1000, T(SEQ(ii).I(end))*1000];
+    % Cluster stat too
+    [~,p,~,stat]=ttest(mean(A(:,(SEQ(ii).I(1):SEQ(ii).I(end))),2));
+    SEQ(ii).p=p;
+    SEQ(ii).stat=stat.tstat;
+end
+
+SSData.SEQ=SEQ;
+%---------------------------------------------------------------------%
+    
+% a=get(gca,'Ylim'); alow=210; ahgh=310; acol=[.9 .9 .9];
+% hline=patch([alow ahgh ahgh alow alow],[a(1) a(1) a(2) a(2) a(1)],acol,'EdgeColor','none','FaceAlpha',1);
+% 
+% a=get(gca,'Ylim'); alow=400; ahgh=800; acol=[.9 .9 .9];
+% hline=patch([alow ahgh ahgh alow alow],[a(1) a(1) a(2) a(2) a(1)],acol,'EdgeColor','none','FaceAlpha',1); 
+
 hline=refline(0,0); set(hline,'color',[.7 .7 .7]); set(hline,'linewidth',hwidth);
 hline=line([0 0],[get(gca,'Ylim')]); set(hline,'color',[.7 .7 .7]); set(hline,'linewidth',hwidth);
-% hline=line([-240 -240],[get(gca,'Ylim')]); set(hline,'color',[.7 .7 .7],'linestyle',':'); set(hline,'linewidth',3);
+
+
 
 for ii=1:length(b)
     if sum(tstruct.subj)>1
         plot(t.*1000,mean(b{ii}),'color',cset{ii},'linewidth',wset(ii),'linestyle',lset{ii}); hold on;
+        % Add SE confidence on
+        if tstruct.SE.on==1
+            if sum(SErange==ii)>0
+                xconf=[t.*1000 t(end:-1:1)*1000];
+                Y=mean(b{ii}); Yerr=std(b{ii})./sqrt(size(b{ii},1));
+                yconf=[Y+Yerr Y(end:-1:1)-Yerr(end:-1:1)];
+                p=fill(xconf,yconf,cset{ii});
+                p.FaceAlpha=0.2;
+                p.EdgeColor='none';
+                clear xconf Y yconf p;
+
+            end
+        end
     else
         plot(t.*1000,b{ii},'color',cset{ii},'linewidth',wset(ii),'linestyle',lset{ii}); hold on;
     end
@@ -142,7 +238,8 @@ end
 
 box off;
 set(gca,'TickDir','out'); set(gca,'XLim',[t(1)*1000 t(end)*1000])
-legend(tstruct.legend,'location','SouthEast'); 
+legend(tstruct.legend,'location','SouthEast'); grid;
+set(gca,'Layer','Top'); 
 end
 %=========================================================================%
 % T figure
@@ -174,7 +271,8 @@ plot(t*1000,c1.tstat(i1),'k','linewidth',2);
 set(gca,'XLim',[t(1)*1000 t(end)*1000])
 
 xlabel('Time (s)','FontSize',14); ylabel('T-value','FontSize',14);
-set(gca,'FontSize',14); grid;
+set(gca,'FontSize',14); 
+
 
 end
 %=========================================================================%
@@ -220,7 +318,6 @@ set(gca,'TickDir','out'); set(gca,'XLim',[t(1)*1000 t(end)*1000])
 
 end
 
-SSData.b=b;
 %=========================================================================%
 % T figure
 %=========================================================================%
@@ -229,7 +326,7 @@ SSData.b=b;
 if tstruct.fig(5)==1
  
 for ii=1:length(DATA)
-    figure(1+ii); set(gcf,'color','w');
+    figure(2+ii); set(gcf,'color','w');
     imagesc(DATA{ii}); L=get(gca,'CLim');
     if L(1)<0 && L(2)>0, set(gca,'CLim',[-abs(max(L)) max(L)]);
     elseif L(1)<0 && L(2)<0, set(gca,'Clim',[L(1) 0]);
